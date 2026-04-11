1d90253166b87b4b2e965510a572d5f478c66a61
Srayan Jana <valorzard.kiko@outlook.com>
2026-04-10 22:36:12 -0700

    basic fna setup finished

--- TiredFNA/Program.cs
+++ TiredFNA/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Xna.Framework;
+
+static class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        using (Game g = new Game())
+        {
+            new GraphicsDeviceManager(g);
+            g.Run();
+        }
+    }
+}
