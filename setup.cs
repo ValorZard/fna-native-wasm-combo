2c05351a2837ff4f44445902d6fda0af8b3988b2
Srayan Jana <valorzard.kiko@outlook.com>
2026-04-11 20:49:43 -0700

    add setup script

--- setup.cs
+++ setup.cs
@@ -1,1 +1,13 @@
-System.Diagnostics.Process.Start("git", "clone https://github.com/FNA-XNA/FNA --recursive -b 26.04 FNANative");
+using System.Diagnostics;
+
+var nativeClone = new Process();
+var wasmClone = new Process();
+nativeClone.StartInfo.FileName = "git";
+nativeClone.StartInfo.Arguments = "clone https://github.com/FNA-XNA/FNA --recursive -b 26.04 FNANative";
+nativeClone.Start();
+wasmClone.StartInfo.FileName = "git";
+wasmClone.StartInfo.Arguments = "clone https://github.com/FNA-XNA/FNA --recursive -b 26.04 FNAWasm";
+wasmClone.Start();
+nativeClone.WaitForExit();
+wasmClone.WaitForExit();
+Console.WriteLine("Done!");
