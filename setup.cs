1d6308d175f8f1ce3cc048960ec329a8b2792707
Srayan Jana <valorzard.kiko@outlook.com>
2026-04-11 22:39:19 -0700

    port wget call to csharp

--- setup.cs
+++ setup.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net.Http;
 
 var nativeClone = new Process();
 var wasmClone = new Process();
@@ -24,3 +25,45 @@ var nativePatch = new Process
 nativePatch.Start();
 nativePatch.WaitForExit();
 Console.WriteLine("Finished applying patches");
+
+Console.WriteLine("Now copying files...");
+if (Directory.Exists("FNAWasmRunner\\statics"))
+{
+    Directory.Delete("FNAWasmRunner\\statics", true);
+}
+try
+{
+    Directory.CreateDirectory("FNAWasmRunner\\statics");
+    using var client = new HttpClient();
+
+    string staticsRelease = "eb111fb8-7474-4f75-a1b7-848fc6293aa5"; 
+
+    string baseUrl = $"https://github.com/r58Playz/FNA-WASM-Build/releases/download/{staticsRelease}";
+    string outputDir = "FNAWasmRunner\\statics";
+
+    string[] files =
+    [
+        "FAudio.a",
+        "FNA3D.a",
+        "libmojoshader.a",
+        "SDL3.a"
+    ];
+
+    foreach (string file in files)
+    {
+        string sourceUrl = $"{baseUrl}/{file}";
+        string destinationPath = Path.Combine(outputDir, file);
+
+        Console.WriteLine($"Downloading {file}...");
+        using HttpResponseMessage response = await client.GetAsync(sourceUrl, HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+
+        await using Stream source = await response.Content.ReadAsStreamAsync();
+        await using FileStream destination = File.Create(destinationPath);
+        await source.CopyToAsync(destination);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error copying files: {e}");
+}
